592ef8cd223e1084eabe8e88c165e0bcd49369a3
unknown <eliastinuk@gmail.com>
2024-05-06 09:20:46 -0300

    se termino el problema diaSemana

---
+++ assets/js/diaSemana.js
@@ -0,0 +1,28 @@
+let semana= parseInt(prompt("Dime un numero del 1 al 7"))
+if (semana>=1 && semana<=1){
+    console.log("Hoy es lunes")
+}
+
+else if (semana>=2 && semana<=2){
+    console.log("Hoy es Martes")
+}
+
+else if (semana>=3 && semana<=3){
+    console.log("Hoy es miercoles")
+}
+else if (semana>=4 && semana<=4){
+    console.log("Hoy es Jueves")
+}
+else if (semana>=5 && semana<=5){
+    console.log("Hoy es Viernes")
+}
+else if (semana>=6 && semana<=6){
+    console.log("Hoy es Sabado")
+}
+else if (semana>=7 && semana<=7){
+    console.log("Hoy es Domingo")
+}
+
+else{
+    console.log("Numero Invalido")
+}
